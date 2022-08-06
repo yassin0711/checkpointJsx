a57519de1fc349ebe550ce58d7454d56cb0ee772
yassin0711 <99025891+yassin0711@users.noreply.github.com>
2022-08-06 10:35:23 +0100

    checkpointJsx

---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from "react";
+const = (firstName,secondName ) =>{
+let fullName = `${firstName} ${secondName}` ;
+
+return (
+    <p>{fullName}</p>
+   ); }  
+
+
+export default fullName
